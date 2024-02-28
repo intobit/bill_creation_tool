30dc4e3508366c7d55469766ff6aca78893db3f5
Höck Alexander <alexander.hoeck@edu.fh-joanneum.at>
2024-02-28 23:21:27 +0100

    added last create bill button and input field for file name

--- gui.py
+++ gui.py
@@ -6,16 +6,20 @@ from flet import (
     Page,
     Row,
     Text,
+    TextField,
     icons,
 )
 
 
 def main(page: Page):
     page.title = "Bill Creation Tool"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
     selected_file_name = Text()
     selected_path = Text()
     output_path = Text()
+    txt_name = TextField(label="Your file name")
 
     def pick_input_file(e: FilePickerResultEvent):
         if e.files:
@@ -32,6 +36,15 @@ def main(page: Page):
 
     get_directory_dialog = FilePicker(on_result=get_output_directory)
 
+    def filename_input(e):
+        if not txt_name.value:
+            txt_name.error_text = "Please enter your file name"
+            page.update()
+        else:
+            name = txt_name.value
+            page.clean()
+            page.add(ft.Text(f"Test, {name}!"))
+
     page.overlay.extend([pick_files_dialog, get_directory_dialog])
 
     page.add(
@@ -43,13 +56,14 @@ def main(page: Page):
                         icon=icons.UPLOAD_FILE,
                         on_click=lambda _: pick_files_dialog.pick_files()
                     ),
-                    margin=10,
-                    padding=10,
                     alignment=ft.alignment.center,
                     bgcolor=ft.colors.GREEN_100,
+                    margin=10,
+                    padding=10,
                     width=250,
                     height=100,
                     border_radius=30,
+
                 ),
                 ft.Container(
                     content=selected_file_name,
@@ -61,7 +75,6 @@ def main(page: Page):
                 ),
 
             ],
-            alignment = ft.MainAxisAlignment.CENTER,
         ),
         Row(
             [
@@ -89,7 +102,32 @@ def main(page: Page):
                     border_radius=10,
                 ),
             ],
-            alignment=ft.MainAxisAlignment.CENTER,
+        ),
+        Row(
+            [
+                ft.Container(
+                    ElevatedButton(
+                        "Create Bill",
+                        icon=icons.CREATE_OUTLINED,
+                        on_click=filename_input,
+                        disabled=page.web,
+                    ),
+                    margin=10,
+                    padding=10,
+                    alignment=ft.alignment.center,
+                    bgcolor=ft.colors.INDIGO_100,
+                    width=250,
+                    height=100,
+                    border_radius=30,
+                ),
+                ft.Container(
+                    content=txt_name,
+                    alignment=ft.alignment.center,
+                    bgcolor=ft.colors.INDIGO_50,
+                    width=500,
+                    height=50,
+                ),
+            ],
         ),
     )
 
